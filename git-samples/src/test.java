ef0db8e16ecc230d80a87717faa726c7daed8757
KarthigaVijaykumar <karthigamadhu8599@gmail.com>
2021-02-05 16:55:11 +0530

    deleted some lines in eclispse

--- git-samples/src/test.java
+++ git-samples/src/test.java
@@ -5,7 +5,5 @@ public class test {
 		System.out.println("Made some changes in test file of git-samples to demonstrate merge conflict");
 		System.out.println("making some more changes to demonstrate merge conflicts");
 		System.out.println("finish");
-		System.out.println("added new branch and made changes in test file");
-		System.out.println("made some more changes");
 	}
 }
