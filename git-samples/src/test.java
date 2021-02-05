b26eb54ec1c66a4f558e9f9570ead9223f5a44df
KarthigaVijaykumar <78353857+KarthigaVijaykumar@users.noreply.github.com>
2021-02-05 10:49:39 +0530

    made some more changes in test file

--- git-samples/src/test.java
+++ git-samples/src/test.java
@@ -3,5 +3,6 @@ public class test {
 	//test
 	public static void main(String[] args) {
 		System.out.println("Made some changes in test file of git-samples to demonstrate merge conflict");
+		System.out.println("making some more changes to demonstrate merge conflicts");
 	}
 }
